8394efaf8264f261a0066d470becb637ddde94b9
JudasM <judasmohlala2012@gmail.com>
2025-01-19 20:48:47 +0200

    the client can send messages to the server and see if the server can respond

--- src/main/java/Client/ClientSide.java
+++ src/main/java/Client/ClientSide.java
@@ -1,35 +1,50 @@
 package Client;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ClientSide {
     public static void main(String[] args) {
 
-        String ipAddress = "localhost";
-        InetAddress address = null;
-        Socket socket = null;
-        PrintWriter writer = null;
+       String ipAddress = "localhost";
+       InetAddress address = null;
+       Socket socket = null;
+       PrintWriter writer = null; // for writing
+       BufferedReader reader = null ; // for reading
+        String clientMessage, serverMessage ;
+        Scanner scanner = new Scanner(System.in);
+
         try {
-            //address of machine you want to talk to...
             address = InetAddress.getByName(ipAddress);
-            System.out.println("IP Address: "+ address);
+            System.out.println("Server address: "+ address);
 
-            //a socket for communication
-            socket = new Socket(address, 8081);
-            System.out.println("Socket: "+socket);
+            //socket
+            socket = new Socket(address,8081);
+            System.out.println("Cumminication cahnnel :" +socket);
 
-            //wrting stream, that sends data/infor to the server
+            // reading and writing streams
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
 
-            //sending data to the server
-            writer.println("----Hello Server---");
+            // data being sent
 
+            System.out.println("Enter clientMessage: ");
+            clientMessage =  scanner.nextLine();
+
+            //writing to server
+            writer.println(clientMessage);
+            if(clientMessage.equalsIgnoreCase("ok")) {
+                System.out.println("Communication terminated");
+                System.exit(1);
+            }
+
+            //messge from server
+            serverMessage = reader.readLine();
+            System.out.println("Message from Server: "+serverMessage);
 
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
@@ -38,14 +53,12 @@ public class ClientSide {
         }
         finally {
             try {
-                System.out.println("Closing communitcation socket");
+                System.out.println("Communication Closing......");
                 socket.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
 
-
     }
-
 }
