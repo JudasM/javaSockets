7e59071854162e9b7a7fd542763ca70cb22d6460
JudasM <judasmohlala2012@gmail.com>
2025-01-19 20:48:16 +0200

    the server is able to respond to the client.

--- src/main/java/Server/ServerSide.java
+++ src/main/java/Server/ServerSide.java
@@ -1,46 +1,57 @@
 package Server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerSide {
     public static void main(String[] args) {
 
         ServerSocket serverSocket = null;
-        Socket socket = new Socket();
+        Socket socket = null;
         BufferedReader reader = null;
-        String message;
+        PrintWriter writer = null;
+        String clientMessage, serverResponse;
+        Scanner scanner = new Scanner(System.in);
 
         try {
-            //server socket that the server will listen on port 8080
+            //starting the server, and listening on port 8081
             serverSocket = new ServerSocket(8081);
-            System.out.println("Server Started....");
+            System.out.println("Server started: "+serverSocket);
 
-            //wait for connection
+            //waiting for communmocation from client
             socket = serverSocket.accept();
-            System.out.println("Connection established..: "+socket);
+            System.out.println("Communication established");
 
-            //creatuing a reading stream, to read data sent/recieved
+            //readign and wrtitng streams
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
 
-            //reading data
-            message = reader.readLine();
+            while (true) {
 
-            // display message
-            System.out.println("Message recieved from client: "+message);
+                //reading message
+                clientMessage = reader.readLine();
 
+                if (clientMessage.equalsIgnoreCase("ok")) {
+                    break;
+                }else  {
+                    System.out.println("Client Message: " +clientMessage);
+                    serverResponse = scanner.nextLine();
 
+                    writer.println(serverResponse);
+
+                }
+
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         finally {
-
             try {
-                System.out.println("Closing the socket.....");
+                System.out.println("Closing.....");
                 socket.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
